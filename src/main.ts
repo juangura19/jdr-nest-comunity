90e10b6e8f914f80f8298a5a674e20b408f4aecf
Juan Gutiérrez <juangura19@gmail.com>
2025-08-07 17:50:34 -0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,15 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/community')
+  app.enableCors({
+    origin: [
+      'http://localhost:4200', // Angular local
+      'https://gentle-glacier-0912d220f.2.azurestaticapps.net'  // si tienes prod
+    ],
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
+    credentials: true
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
